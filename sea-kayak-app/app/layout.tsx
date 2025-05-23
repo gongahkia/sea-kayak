d81295edfe3323d16e5130c5ead549ed8a0d3a36
gongahkia <angryapplegravy@gmail.com>
2025-05-23 22:31:49 +0800

    feat(app): added

--- sea-kayak-app/app/layout.tsx
+++ sea-kayak-app/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Sea Kayak",
-  description: "Explore the ocean through sea kayaking adventures",
+  description: "Legal News RSS Feed for the wandering Sea Kayaker.",
 }
 
 export default function RootLayout({
